6762ef720ce9924fe8357e7d3d19581198292c1a
unknown <inavruzov777@gmail.com>
2023-10-04 11:10:07 +0500

    first commit

---
+++ database/migrations/2023_04_09_204805_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Post;
+use App\Http\Controllers\PostController;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('short_content');
+            $table->text('content');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
